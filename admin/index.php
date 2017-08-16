856e90d250e502944d55dd7d4cfa4654b23709aa
Bahlul Siddiquee Tausif <bahlul.tausif@gmail.com>
2017-08-16 03:27:07 -0700

    admin section just started/login_session

---
+++ admin/index.php
@@ -0,0 +1,18 @@
+<?php include('inc/header.php'); ?>
+        
+        <!--Header-->
+    
+<?php include('inc/sidebar.php'); ?> 
+        
+            <!--Sidebar-->
+
+        <div class="grid_10">
+		
+            <div class="box round first grid">
+                <h2> Dashbord</h2>
+                <div class="block">               
+                  Welcome admin panel        
+                </div>
+            </div>
+        </div>
+        <?php include('inc/footer.php'); ?> 
